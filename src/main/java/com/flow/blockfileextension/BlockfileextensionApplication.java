cbea15e528e8d8af68798b32b5984c099954cc30
namji95 <skawlgns07@naver.com>
2024-08-13 20:22:07 +0900

    readme 작성

---
+++ src/main/java/com/flow/blockfileextension/BlockfileextensionApplication.java
@@ -0,0 +1,13 @@
+package com.flow.blockfileextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockfileextensionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BlockfileextensionApplication.class, args);
+  }
+
+}

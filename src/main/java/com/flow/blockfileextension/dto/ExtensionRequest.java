93e28f521f52edefde29c5a542437e0dcb6c8b53
namji95 <skawlgns07@naver.com>
2024-08-19 17:30:23 +0900

    refactor: 요청 값에 대한 예외처리
    요청 값의 validation 처리에 대한 예외 Custom Exception을 활용하여 수정 후 프론트에서 확인할 수 있도록 수정

--- src/main/java/com/flow/blockfileextension/dto/ExtensionRequest.java
+++ src/main/java/com/flow/blockfileextension/dto/ExtensionRequest.java
@@ -9,8 +9,8 @@ import lombok.Getter;
 public class ExtensionRequest {
 
   @NotBlank(message = "확장자 선택 또는 작성해야 합니다.")
-  @Pattern(regexp = "^[a-zA-Z]*$", message = "확장자는 영어만 작성할 수 있습니다.")
-  @Size(min = 1, max = 20, message = "글자수는 1자 ~ 20자내로 작성해주시기 바랍니다.")
+  @Pattern(regexp = "^[a-zA-Z]*$", message = "영어만 작성할 수 있습니다.")
+  @Size(min = 1, max = 20, message = "1자 ~ 20자내로 작성해주시기 바랍니다.")
   private String extensionName;
 
   private Boolean bool;
--- src/main/java/com/flow/blockfileextension/exception/GlobalCustomException.java
+++ src/main/java/com/flow/blockfileextension/exception/GlobalCustomException.java
@@ -1,6 +1,10 @@
 package com.flow.blockfileextension.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,5 +14,24 @@ public class GlobalCustomException {
   @ExceptionHandler(CustomException.class)
   protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
       return ErrorResponse.toResponseEntity(e.getErrorCode());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> processValidationException(MethodArgumentNotValidException e) {
+    BindingResult bindingResult = e.getBindingResult();
+
+    StringBuilder builder = new StringBuilder();
+    if (bindingResult.getFieldError().getField().equals("extensionName")) {
+      for (FieldError fieldError : bindingResult.getFieldErrors()) {
+        builder
+            .append("확장자 이름은 ")
+            .append(fieldError.getDefaultMessage())
+            .append("\n")
+            .append("입력된 값 : ")
+            .append(fieldError.getRejectedValue());
+      }
+    }
+
+    return new ResponseEntity<>(builder.toString(), HttpStatus.BAD_REQUEST);
   }
 }
